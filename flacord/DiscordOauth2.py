f9e82401e2cba6ddae1a6b000c1a11a535b005ff
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2023-03-07 04:59:44 +0900

    Update DiscordOauth2.py

--- flacord/DiscordOauth2.py
+++ flacord/DiscordOauth2.py
@@ -1,1 +1,10 @@
+from flacord import scope
 
+class DiscordOauth2Session:
+    def __init__(self, app):
+        self.app = app
+        self.base_url = 'https://discord.com/api/v9'
+
+
+    def Login(self,scope=scope.all()):
+        '%20'.join(scope)

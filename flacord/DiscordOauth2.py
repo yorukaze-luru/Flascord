4b23c6a0bc29d21f3528f83c859795b899db2a03
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2023-03-14 08:44:33 +0900

    Update DiscordOauth2.py

--- flacord/DiscordOauth2.py
+++ flacord/DiscordOauth2.py
@@ -4,21 +4,28 @@ from flask import request, session, redirect, current_app
 class DiscordOauth2Session:
     def __init__(self, app):
         self.app = app
+        self.config = self.app.config
         self.base_url = 'https://discord.com/api/v9'
         self.oauth_url = self.base_url + '/oauth2/'
         self.get_token_url = self.oauth_url + '/token'
 
-    def Login(self,scope=scope.all()):
-        '%20'.join(scope)
+    def login(self,scope=scope.all()):
+        scope = '%20'.join(scope)
+        login_url = f"{self.oauth_url}authorize?client_id={self.config['DISCORD_CLIENT_ID']}&redirect_uri={self.config['DISCORD_REDIRECT_URI']}&response_type=code&scope={scope}"
+        return redirect(login_url)
 
-    def callback():
+    def callback(self):
         authorization_code = request.args.get("code")
 
-        request_postdata = {'client_id': app.config['DISCORD_CLIENT_ID'], 'client_secret': app.config['DISCORD_CLIENT_SECRET'], 'grant_type': 'authorization_code', 'code': authorization_code, 'redirect_uri': app.config['DISCORD_REDIRECT_URI']}
-        accesstoken_request = requests.post('https://discord.com/api/oauth2/token', data=request_postdata)
+        request_postdata = {'client_id': self.config['DISCORD_CLIENT_ID'], 'client_secret': self.config['DISCORD_CLIENT_SECRET'], 'grant_type': 'authorization_code', 'code': authorization_code, 'redirect_uri': self.config['DISCORD_REDIRECT_URI']}
+        accesstoken_request = requests.post(url=self.get_token_url, data=request_postdata)
 
         responce_json = accesstoken_request.json()
 
         session['access_token'] = responce_json['access_token']
-    
-        return redirect(url_for(".loading"))
+        session['token_type'] = responce_json['token_type']
+        session['expires_in'] = responce_json['expires_in']
+        session['refresh_token'] = responce_json['refresh_token']
+        session['scope'] = responce_json['scope']
+        
+        return

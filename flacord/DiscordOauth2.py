ec335e7372b1ca813cc037c6e0bedd0f6d6c9123
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2023-03-14 07:43:37 +0900

    Update DiscordOauth2.py

--- flacord/DiscordOauth2.py
+++ flacord/DiscordOauth2.py
@@ -1,4 +1,5 @@
 from flacord import scope
+from flask import request, session, redirect, current_app
 
 class DiscordOauth2Session:
     def __init__(self, app):
@@ -9,3 +10,15 @@ class DiscordOauth2Session:
 
     def Login(self,scope=scope.all()):
         '%20'.join(scope)
+
+    def callback():
+        authorization_code = request.args.get("code")
+
+        request_postdata = {'client_id': app.config['DISCORD_CLIENT_ID'], 'client_secret': app.config['DISCORD_CLIENT_SECRET'], 'grant_type': 'authorization_code', 'code': authorization_code, 'redirect_uri': app.config['DISCORD_REDIRECT_URI']}
+        accesstoken_request = requests.post('https://discord.com/api/oauth2/token', data=request_postdata)
+
+        responce_json = accesstoken_request.json()
+
+        session['access_token'] = responce_json['access_token']
+    
+        return redirect(url_for(".loading"))

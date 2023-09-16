ae355ceb3f3dacf84baf2ed14c10daec5cec7aa5
yorukaze-luru <46132263+yorukaze-luru@users.noreply.github.com>
2023-09-16 19:27:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,32 @@ import os
 
 VERSION = '0.1.1'
 
-def load_requires_from_file(fname):
-    if not os.path.exists(fname):
-        raise IOError(fname)
-    return [pkg.strip() for pkg in open(fname, 'r')]
+def load_requires_from_file():
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+    return requirements
+
+def load_readme_from_file():
+    with open('README.md', 'r', encoding='utf-8') as fp:
+        readme = fp.read()
+    return readme
 
 setup(
     name='Flascord',
     author='Yorukaze LURU',
-    license='MIT'
+    license='MIT',
     url="https://github.com/yorukaze-luru/Flascord",
     version=VERSION,
     packages=find_packages(),
     include_package_data=True,
-    KEYWORDS = 'flask discord oauth2'
+    KEYWORDS = 'flask discord oauth2',
+    long_description=load_readme_from_file(),
+    long_description_content_type='text/markdown',
     CLASSIFIERS=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-    ]
-    install_requires=load_requires_from_file('requirements.txt'),
+    ],
+    install_requires=load_requires_from_file(),
 )
